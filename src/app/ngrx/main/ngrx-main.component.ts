c39df337276c263db1829b37d7950ae3fe0df910
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2020-07-09 10:08:17 +0200

    after removing unused folders

--- src/app/ngrx/main/ngrx-main.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from "@angular/core";
-
-@Component({
-    selector: 'ngrx-main',
-    templateUrl: './ngrx-main.component.html',
-    styleUrls: ['./ngrx-main.component.scss']
-
-})
-export  class  NgrxMainComponent{}
--- src/app/reactive/main/reactive-main.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from "@angular/core";
-
-@Component({
-  selector: 'reactive-main',
-  templateUrl: './reactive-main.component.html',
-  styleUrls: ['./reactive-main.component.scss']
-})
-export class ReactiveMainComponent {
-}
